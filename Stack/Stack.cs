e4d2b0139458a2a257011bb80f575a89c9d2e9d7
Sarvesh <sarvesh.upadhyay@tavant.com>
2015-06-30 12:33:03 +0530

    Stack Example

---
+++ Stack/Stack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stack
+{
+    public class Stack
+    {
+        int[] itemCollection = new int[5];
+        int index = -1;
+
+        public void Push(int item)
+        {
+            index++;
+            itemCollection[index]=item;
+        }
+
+        public  IEnumerable<int> Pop()
+        {
+            for (int i = index; i >= 0; i--)
+            {
+                yield return itemCollection[i];
+            }
+        }
+    }
+
+
+}
